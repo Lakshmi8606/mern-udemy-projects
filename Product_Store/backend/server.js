5e42281a7d49c9ec88d1252199a11ee60f7e7c77
Lakshmi Raj <lakshmiraj9847506759@gmail.com>
2025-08-26 11:52:58 +0530

    Initialize frontend with Vite and Chakra UI.

--- Product_Store/backend/server.js
+++ Product_Store/backend/server.js
@@ -10,9 +10,10 @@ dotenv.config({ path: "./.env" });
 console.log("Mongo URI:", process.env.MONGO_URI);
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(express.json()); //allows us to accept json data in the body
 app.use("/api/products", productRoutes);
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:" + PORT);
 });
